f77ce949f8b3586a1b73669d6ab56f0949f59925
Feddy096 <marius.jenseen@gmail.com>
2026-02-27 13:27:53 +0100

    La til et mood system og personlighet system

---
+++ Assets/Scripts/CustomerScript.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class CustomerScript : MonoBehaviour
+{
+    public float moodMeter = 100;
+    public enum Personalities
+    {
+        Chill, Normal, Angry, Karen
+    }
+    public enum ReceptionTasks
+    {
+        BookIn, BookOut, BuyRoom
+    }
+
+    private ReceptionTasks currentTask;
+    private Personalities personality;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        moodMeter -= Random.Range(0.1f, 60f);
+        int index = Random.Range(0, 3);
+
+        // Setter hvilken ting du må gjøre for kunden
+        switch (index)
+        {
+            case 0: currentTask = ReceptionTasks.BookIn; break;
+            case 1: currentTask = ReceptionTasks.BookOut; break;
+            case 2: currentTask = ReceptionTasks.BuyRoom; break;
+        }
+
+        //Bestemmer hvor fort moodMeter går ned
+        index = Random.Range(0, 4);
+        switch (index)
+        {
+            case 0: personality = Personalities.Chill; break;
+            case 1: personality = Personalities.Normal; break;
+            case 2: personality = Personalities.Angry; break;
+            case 3: personality = Personalities.Karen; break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (personality)
+        {
+            case Personalities.Chill: moodMeter -= 1 * Time.deltaTime; break;
+            case Personalities.Normal: moodMeter -= 2 * Time.deltaTime; break;
+            case Personalities.Angry: moodMeter -= 4 * Time.deltaTime; break;
+            case Personalities.Karen: moodMeter -= 6 * Time.deltaTime; break;
+        }
+    }
+}
