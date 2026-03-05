6bccf0ebb20ac761cf6b2b73cf545c91f576dc02
EivindB <ekbbak@gmail.com>
2026-03-05 15:00:16 +0100

    Bynt med at du kan gå tilbake til forrige menu

--- Assets/Scripts/FollowCursor.cs
+++ Assets/Scripts/FollowCursor.cs
@@ -6,7 +6,10 @@ public class FollowCursor : MonoBehaviour
 
     public float followstrength = 1;
     public Camera cam;
+    public Camera cam2;
+    public bool goBackToPlayer;
     public float max;
+    public CharacterMainScript PlayerScript;
     
     void Update()
     {
@@ -15,9 +18,28 @@ public class FollowCursor : MonoBehaviour
         MousePos.z = Mathf.Abs(cam.transform.position.z);
         var ViewPos = cam.ScreenToWorldPoint(MousePos);
         transform.rotation = Quaternion.Euler(Mathf.Clamp(((ViewPos.y-transform.position.y)*-1)*followstrength,max*-1,max),Mathf.Clamp(((ViewPos.x-transform.position.x)/1.75f)*followstrength,max*-1,max), 0);
+
+        var keyboard = Keyboard.current;
+
         
+        if (cam.targetDisplay == 0)
+        {
+            if (keyboard.eKey.isPressed)
+            {
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+                cam.targetDisplay = 1;
+                cam2.targetDisplay = 0;
+                if (goBackToPlayer)
+                {
+                    PlayerScript.enabled = true;
+                    Cursor.visible = false;
+                }
+            }
+        }
         //Debug.Log(ViewPos);
     }
+    
     
     // legg te en variable for main andre kamera og når du trykke på e så bli du sendt tilbake plus en bool om det ska vis cursor eller ikke
 }
