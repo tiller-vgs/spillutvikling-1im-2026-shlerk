7e478eac1b09ddce007ab4a662b00907b1d25e5a
Feddy096 <marius.jenseen@gmail.com>
2026-03-05 15:05:17 +0100

    QueueManager spiller nå av lyden på kundene på et tilfeldig interval

--- Assets/Scripts/QueueManager.cs
+++ Assets/Scripts/QueueManager.cs
@@ -1,17 +1,30 @@
+using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 
 public class QueueManager : MonoBehaviour
 {
-    public GameObject spritePrefab;
+    public GameObject customerPrefab;
 
     public int antallKunda;
     public Vector2 offset;
+    public Camera Cam;
+
+    public List<GameObject> customers;
     
     private Vector3 SpawnPoint;
+
+    private float timer = 0;
+    private float timeUntilVoiceActivation = 0;
     
-    
+    // Hvem gjorde dette her 😭
     private int i = 0;
-    
+
+    void Awake()
+    {
+        customers = new List<GameObject>();
+    }
+
     void Start()
     {
         SpawnPoint = transform.position;
@@ -19,7 +32,8 @@ public class QueueManager : MonoBehaviour
         for (i=0;i <= antallKunda; i++)
         {
             Debug.Log(SpawnPoint);
-            Instantiate(spritePrefab, SpawnPoint, transform.rotation);
+            
+            customers.Add(Instantiate(customerPrefab, SpawnPoint, transform.rotation));
             SpawnPoint = new (SpawnPoint.x+offset.x, SpawnPoint.y, SpawnPoint.z+offset.y);
         }
     }
@@ -27,6 +41,20 @@ public class QueueManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Cam.targetDisplay == 0)
+        {
+            timer += Time.deltaTime;
+            if (timer >= timeUntilVoiceActivation)
+            {
+                timeUntilVoiceActivation = Random.Range(3f, 10f);
+                customers[Random.Range(0, antallKunda)].GetComponent<CustomerScript>().playRandomVoiceLine();
+                timer = 0;
+            }
+        }
+    }
+
+    private IEnumerator Voicelines()
+    {
+        yield return new WaitForSeconds(2);
     }
 }
