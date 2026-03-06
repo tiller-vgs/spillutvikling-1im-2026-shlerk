97c68575aca148f72ddc29ac2cd3ad25f3839751
Feddy096 <marius.jenseen@gmail.com>
2026-03-06 11:13:47 +0100

    laget layout for hovedmeny og laget funksjonalitet for start og quit knapp

--- Assets/Scripts/FollowCursor.cs
+++ Assets/Scripts/FollowCursor.cs
@@ -7,6 +7,7 @@ public class FollowCursor : MonoBehaviour
     public float followstrength = 1;
     public Camera cam;
     public float max;
+    private Vector2 aspectRatio = new Vector2(16, 9);
     
     void Update()
     {
@@ -14,7 +15,7 @@ public class FollowCursor : MonoBehaviour
         Vector3 MousePos = Mouse.current.position.ReadValue();
         MousePos.z = Mathf.Abs(cam.transform.position.z);
         var ViewPos = cam.ScreenToWorldPoint(MousePos);
-        transform.rotation = Quaternion.Euler(Mathf.Clamp(((ViewPos.y-transform.position.y)*-1)*followstrength,max*-1,max),Mathf.Clamp(((ViewPos.x-transform.position.x)/1.75f)*followstrength,max*-1,max), 0);
+        transform.rotation = Quaternion.Euler(Mathf.Clamp((((ViewPos.y-transform.position.y)/aspectRatio.y)*-1)*followstrength,max*-1,max),Mathf.Clamp(((ViewPos.x-transform.position.x)/aspectRatio.x)*followstrength,max*-1,max), 0);
         
         //Debug.Log(ViewPos);
     }
