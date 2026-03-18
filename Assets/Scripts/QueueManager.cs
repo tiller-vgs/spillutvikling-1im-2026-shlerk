0011ffe24703dd3763b08f7616f38bec5682f024
Feddy096 <marius.jenseen@gmail.com>
2026-03-18 13:25:19 +0100

    la til en moodmeter display PROTOTYPE og gjorde customers til en faktisk queue istedet for en list

--- Assets/Scripts/QueueManager.cs
+++ Assets/Scripts/QueueManager.cs
@@ -10,7 +10,7 @@ public class QueueManager : MonoBehaviour
     public Vector2 offset;
     public Camera Cam;
 
-    public List<GameObject> customers;
+    public Queue<GameObject> customers;
     
     private Vector3 SpawnPoint;
 
@@ -22,18 +22,18 @@ public class QueueManager : MonoBehaviour
 
     void Awake()
     {
-        customers = new List<GameObject>();
+        customers = new Queue<GameObject>();
     }
 
     void Start()
     {
         SpawnPoint = transform.position;
         //lage like mange kunder som antall kunder si
-        for (i=0;i <= antallKunda; i++)
+        for (i=0; i < antallKunda; i++)
         {
             Debug.Log(SpawnPoint);
             
-            customers.Add(Instantiate(customerPrefab, SpawnPoint, transform.rotation));
+            customers.Enqueue(Instantiate(customerPrefab, SpawnPoint, transform.rotation));
             SpawnPoint = new (SpawnPoint.x+offset.x, SpawnPoint.y, SpawnPoint.z+offset.y);
         }
     }
@@ -47,7 +47,9 @@ public class QueueManager : MonoBehaviour
             if (timer >= timeUntilVoiceActivation)
             {
                 timeUntilVoiceActivation = Random.Range(3f, 10f);
-                customers[Random.Range(0, antallKunda)].GetComponent<CustomerScript>().playRandomVoiceLine();
+                if (customers.Count > 0){
+                    customers.Peek().GetComponent<CustomerScript>().playRandomVoiceLine();
+                }
                 timer = 0;
             }
         }
@@ -56,5 +58,10 @@ public class QueueManager : MonoBehaviour
     private IEnumerator Voicelines()
     {
         yield return new WaitForSeconds(2);
+    }
+
+    void UpdateQueuePos()
+    {
+        
     }
 }
